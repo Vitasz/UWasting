38f287cc8df48ea350c0ca6a9cae2184b5073c6d
Vitasz <79321565+Vitasz@users.noreply.github.com>
2022-02-16 22:43:31 +0300

    delete all

--- app/src/main/java/com/example/uwasting/data/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.example.uwasting.data
-
-class Constants {
-    companion object {
-        const val CHANGE_EMAIl = 0
-        const val CHANGE_PASSWORD = 1
-        const val APIurl = "http://192.168.72.13:5000/"
-    }
-}
