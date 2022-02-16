4f760f0c771a6eb1f53d6acff31897e2217a5f46
Vitasz <79321565+Vitasz@users.noreply.github.com>
2022-02-16 22:34:16 +0300

    try fix

---
+++ Controllers/OperationsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : Controller
+    {
+        [Route("/AddOperation")]       
+        [HttpPost]
+        public void AddOperation(int value, string category, DateTime date, int id)
+        {
+            Database.Operations.SaveOperation(value, category, date, id);
+        }
+
+        [Route("/GetOperations")]
+        [HttpGet]
+        public string GetOperations(int UserId)
+        {
+            List<(int id, int Value, string Category, DateTime date)> ans = Database.Operations.LoadOperations(UserId);
+            List<Operation> toret = new List<Operation>();
+            foreach((int id, int Value, string Category, DateTime date) a in ans)
+            {
+                Operation tmp = new Operation();
+                tmp.Id = a.id;
+                tmp.Value = a.Value;
+                tmp.Category = a.Category;
+                tmp.date = a.date;
+                toret.Add(tmp);
+            }
+            return JsonConvert.SerializeObject(toret);
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    [Serializable]
+    public class User
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string email { get; set; }
+        public int id { get; set; }
+    }
+}
