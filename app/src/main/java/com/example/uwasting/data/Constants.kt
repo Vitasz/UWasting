0dcab4a1895be6997128151f30d5328acdafbc82
Vitasz <79321565+Vitasz@users.noreply.github.com>
2022-02-17 21:53:22 +0300

    add api

--- app/src/main/java/com/example/uwasting/data/Constants.kt
+++ app/src/main/java/com/example/uwasting/data/Constants.kt
@@ -4,6 +4,6 @@ class Constants {
     companion object {
         const val CHANGE_EMAIl = 0
         const val CHANGE_PASSWORD = 1
-        const val APIurl = "http://192.168.72.13:5000/"
+        const val APIurl = "https://uwasting.herokuapp.com/"
     }
 }
